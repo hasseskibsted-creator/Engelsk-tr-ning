275b8524d4e8312fac20eece5371285d538d94a9
hasseskibsted-creator <hasseskibsted@gmail.com>
2026-06-03 19:44:56 +0200

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ if adgangskode == "1531":
 
     if api_key:
         genai.configure(api_key=api_key)
-        model = genai.GenerativeModel('gemini-1.5-flash')
+        model = genai.GenerativeModel('gemini-2.0-flash-lite')
 
         st.title("Engelsk B Eksamensforberedelse")
         
